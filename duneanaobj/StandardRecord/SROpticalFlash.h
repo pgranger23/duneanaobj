e3fcbe9353f7d2ac6c8dafb4470207ed2d776e0e
sindhu-ku <kumaran.sindhujha@gmail.com>
2024-08-15 12:37:16 -0500

    rm unecessary pe per channel

--- duneanaobj/StandardRecord/SROpticalFlash.h
+++ duneanaobj/StandardRecord/SROpticalFlash.h
@@ -10,7 +10,7 @@
 
 namespace caf
 {
-  /// \brief Reconstructed optical flash candidate
+  /// \brief optical flash candidate
   class SROpticalFlash
   {
     private:
@@ -20,14 +20,12 @@ namespace caf
     public:
       int    id            = -1;     ///< id of the flash>
       int    tpc_id        = -1;     ///< id of the TPC>
-      bool   on_beam_time  = false;  ///< is the flash on time with the beam?
       double time          = NaN;    ///< time of the flash w.r.t. trigger time time in s
       double time_width    = NaN;    ///< width of the flash in s
       float  total_pe      = NaN;    ///< total photoelectrons recorded by all the channels
-      std::vector<float> pe_per_channel;    ///< number of photoelectrons per channel
       
   };
 
 } // caf
 
-#endif //DUNEANAOBJ_SRRECOOPTICALFLASH_H
+#endif //DUNEANAOBJ_SROPTICALFLASH_H
