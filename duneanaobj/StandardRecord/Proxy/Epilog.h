f2be998f991d82ab14b2e00940e603b1953e0ca2
Luke Pickering <luketpickering@gmail.com>
2024-10-23 11:35:03 +0100

    just explicitly instantiate Proxy<StandardRecord>

--- duneanaobj/StandardRecord/Proxy/Epilog.h
+++ duneanaobj/StandardRecord/Proxy/Epilog.h
@@ -1,15 +1,1 @@
-#include "duneanaobj/StandardRecord/SREnums.h"
-
-// But this also gives us an opportunity to instantiate the template for
-// various DUNE-specific enums that would otherwise be missing symbols.
-namespace caf {
-template class Proxy<Detector>;
-template class Proxy<Generator>;
-template class Proxy<PartEMethod>;
-template class Proxy<ScatteringMode>;
-
-template class Proxy<NDLAR_RECO_STACK>;
-template class Proxy<FD_RECO_STACK>;
-
-template class Proxy<TrueParticleID::PartType>;
-} // namespace caf
+template class caf::Proxy<caf::StandardRecord>;
