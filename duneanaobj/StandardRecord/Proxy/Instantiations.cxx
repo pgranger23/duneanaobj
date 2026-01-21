e7b37defec53d426d337f8f354ebda8e2b8dab0f
Pierre <pierre.granger@cern.ch>
2026-01-21 15:31:44 +0100

    Fix linker duplicate symbol errors by unifying proxy compilation units

--- duneanaobj/StandardRecord/Proxy/Instantiations.cxx
+++ duneanaobj/StandardRecord/Proxy/Instantiations.cxx
@@ -1,6 +1,5 @@
 // This file is the only way BasicTypesProxy.cxx gets compiled at all (the
 // srproxy package doesn't include binaries).
-#include "SRProxy/BasicTypesProxy.cxx"
 
 #include "duneanaobj/StandardRecord/Proxy/SRProxy.h"
 #include "duneanaobj/StandardRecord/Navigate.ixx"
@@ -13,16 +12,16 @@
 namespace caf
 {
   template class Proxy<Detector>;
-  template class Proxy<Generator>;
-  template class Proxy<PartEMethod>;
-  template class Proxy<ScatteringMode>;
+  // template class Proxy<Generator>;
+  // template class Proxy<PartEMethod>;
+  // template class Proxy<ScatteringMode>;
 
-  template class Proxy<NDLAR_RECO_STACK>;
+  // template class Proxy<NDLAR_RECO_STACK>;
   template class Proxy<FD_RECO_STACK>;
-  template class Proxy<NDRecoMatchType>;
+  // template class Proxy<NDRecoMatchType>;
 
-  template class Proxy<TrueParticleID::PartType>;
-  template class Proxy<RecoObjType>;
+  // template class Proxy<TrueParticleID::PartType>;
+  // template class Proxy<RecoObjType>;
 
   template const SRTrueParticleProxy * FindParticle(const SRTruthBranchProxy & truth, const TrueParticleIDProxy & id);
   template const SRTrueInteractionProxy * FindInteraction(const SRTruthBranchProxy & truth,  long int id);
